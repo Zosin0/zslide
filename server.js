6efd80e631f2bc5902d5010b9e746e10b5c53909
Zosin0 <lucaszoser1@gmail.com>
2024-08-21 10:31:57 -0300

    1st version

--- server.js
+++ server.js
@@ -10,9 +10,11 @@ const port = 3000;
 app.use(bodyParser.json());
 app.use(express.static('public')); // Servir arquivos estáticos
 
+let pdfPath = ''; // Variável global para armazenar o caminho do PDF
+
 app.post('/save-content', async (req, res) => {
     const htmlContent = req.body.html;
-    console.log(htmlContent)
+    //console.log(htmlContent);
 
     // Salva o conteúdo HTML em um arquivo
     const filePath = path.join(__dirname, 'content.html');
@@ -22,18 +24,35 @@ app.post('/save-content', async (req, res) => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
 
+    // Definindo o viewport
+    await page.setViewport({ width: 1920, height: 1080 });
+
     // Defina o conteúdo da página
     await page.setContent(htmlContent, { waitUntil: 'networkidle0' });
+
+    // Caminho onde o PDF será salvo temporariamente
+    pdfPath = path.join(__dirname, 'slides.pdf');
     await page.pdf({
-        path: 'slides.pdf',
+        path: pdfPath,
         format: 'A4',
         printBackground: true,
         landscape: true,
+        margin: { top: 0, right: 0, bottom: 0, left: 0 } // Margens ajustadas para zero
     });
 
     await browser.close();
 
     res.send('PDF gerado com sucesso');
+});
+
+
+// Rota GET para baixar o PDF
+app.get('/download-pdf', (req, res) => {
+    if (fs.existsSync(pdfPath)) {
+        res.sendFile(pdfPath);
+    } else {
+        res.status(404).send('PDF não encontrado. Por favor, gere primeiro.');
+    }
 });
 
 app.listen(port, () => {
